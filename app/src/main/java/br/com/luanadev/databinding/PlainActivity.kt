46daabffedc66a890071de6345561609afebf5f2
Luana Souza <luana.souza@ifood.com.br>
2021-01-20 16:13:27 -0300

    contruindo o layout, e configurando o gradle

--- app/src/main/java/br/com/luanadev/databinding/PlainActivity.kt
+++ app/src/main/java/br/com/luanadev/databinding/PlainActivity.kt
@@ -3,9 +3,9 @@ package br.com.luanadev.databinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PlainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_plain)
     }
 }
